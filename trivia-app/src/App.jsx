9bfb36d092412e375d8c51f4239271cf7f720170
samanthafranks03 <rdu5qu@virginia.edu>
2025-04-11 16:28:53 +0200

    Added Question component to display 10 questions from the trivia database

--- trivia-app/src/App.jsx
+++ trivia-app/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
 import './App.css'
+import Question from "./components/Question";
 
 function App() {
-  const API_URL = 'https://the-trivia-api.com/v2/questions';
-  const [questionData, setQuestionData] = useState({});
+  const API_URL = `https://opentdb.com/api.php?amount=10&type=multiple`;
+  const [questionData, setQuestionData] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -27,19 +28,25 @@ function App() {
     };
     fetchQuestionData();
   }, [])
-    if (isLoading){
+  if (isLoading){
       return (
       <>
         <p>Loading</p>
       </>
       );
-    }
+  }
 
 
   return (
-    <>
-    </>
-  )
-}
+      <>
+        <div>
+          <h1>Quiz Questions</h1>
+          {questionData.map((question, index) => (
+            <Question key={index} questionData={question} />
+          ))}
+        </div>
+      </>
+    );
+  }
 
 export default App
